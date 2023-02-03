f6626889e48410922bf306b025ee978b3c855718
H-cyber <alimom9105@gmail.com>
2023-02-02 23:33:04 -0500

    New features added to the source include:
    1- Checking the existence of an active connection with the active interface of the system
    
    2- Checking the output and the error of the loose response after checking the presence of an active connection
    
    3- Removing the inactive connection by the network manager
    
    4- Adding a new connection with the active interface of the system
    
    5 - Activation of the newly created connection
    
    7- Restart the network service to apply DNS name changes
    
    8- Activating the new connection made after restarting the network with new changes

--- DnsChanger.sh
+++ DnsChanger.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
 #
 
-# --check return command output error fun ---#
-check_command() {
-	command
-	if [ $? -eq 0 ]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-#---------------------------------------------#
-
-#------ grep active network interface --------#
-active_device=$(nmcli -t -f device connection show --active)
-
-#------ grep active network connection type --#
-connection_type=$(nmcli -t -f type connection show --active)
-
-#--- Checking the presence of an active interface connection ---#
-check_command sudo nmcli con up $active_device >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-	# --- Deleting the active connection ---#
-	nmcli connection delete type $connection_type con-name $active_device >/dev/null 2>&1
-	# ---- Creating a new connection with an active interface ---#
-	sudo nmcli connection add type $connection_type con-name $active_device >/dev/null 2>&1
-	# --- New connection activation ---#
-	sudo nmcli con up $active_device >/dev/null 2>&1
-else
-	# ---- Activate the existing connection, if any --#
-	sudo nmcli con up $active_device >/dev/null 2>&1
-fi
-
 cat <<EOF
  --==[ ===================================== ]==--
  --==[ Auto DNS changer by Pakrohk! V0.1     ]==--
@@ -54,7 +22,39 @@ EOF
 
 read -p "Enter your desired DNS number : " DNS
 
-NETNAME=$(nmcli -t -f NAME c show --active | head -n 1)
+# --check return command output error fun ---#
+check_command() {
+	command
+	if [ $? -eq 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+#---------------------------------------------#
+
+#------ grep active network interface --------#
+active_device=$(sudo nmcli -t -f device connection show --active)
+
+#------ grep active network connection type --#
+connection_type=$(sudo nmcli -t -f type connection show --active)
+
+#--- Checking the presence of an active interface connection ---#
+check_command sudo nmcli con up $active_device >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	# --- Deleting the active connection ---#
+	sudo nmcli connection delete type $connection_type con-name $active_device >/dev/null 2>&1
+	# ---- Creating a new connection with an active interface ---#
+	sudo nmcli connection add type $connection_type con-name $active_device >/dev/null 2>&1
+	# --- New connection activation ---#
+	sudo nmcli con up $active_device >/dev/null 2>&1
+else
+	# ---- Activate the existing connection, if any --#
+	sudo nmcli con up $active_device >/dev/null 2>&1
+fi
+
+NETNAME=$(sudo nmcli -t -f NAME c show --active | head -n 1)
 
 if [[ $DNS == 1 ]]; then
 	sudo nmcli connection modify $NETNAME ipv4.dns "8.8.8.8 8.8.4.4"
